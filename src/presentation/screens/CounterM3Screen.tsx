9e7f5eac10284aed9ccb2d199419526ddd90e2a8
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-11-08 16:07:47 -0300

    add icon increment

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Text, View } from 'react-native';
 import { globalStyles } from '../theme/global.styles';
 
-import Icon from 'react-native-vector-icons/Ionicons';
+// import Icon from 'react-native-vector-icons/Ionicons';
 
 import { FAB } from 'react-native-paper';
 
@@ -13,13 +13,14 @@ export const CounterM3Screen = () => {
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{count}</Text>
 
-      <Icon name="accessibility-outline" size={35} />
+      {/* <Icon name="accessibility-outline" size={35} /> */}
 
       <FAB
-        label="+1"
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
         style={globalStyles.fab}
+        // label="+1"
+        icon="add-outline"
         // icon={() => <Icon name="accessibility-outline" size={25} />}
       />
     </View>
