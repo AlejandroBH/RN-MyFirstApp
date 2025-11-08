afe3287dd592817703ead8449c68162c38a92df6
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-11-08 15:09:06 -0300

    button component

--- src/presentation/components/shared/PrimaryButton.tsx
+++ src/presentation/components/shared/PrimaryButton.tsx
@@ -1,3 +1,34 @@
-export const PrimaryButton = () => {
-  return <div>PrimaryButton</div>;
+import { Platform, Pressable, StyleSheet, Text } from 'react-native';
+
+interface Props {
+  label: string;
+  onPress?: () => void;
+  onLongPress?: () => void;
+}
+
+export const PrimaryButton = ({ label, onPress, onLongPress }: Props) => {
+  return (
+    <Pressable
+      onPress={() => onPress && onPress()}
+      onLongPress={() => onLongPress && onLongPress()}
+      style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+    >
+      <Text style={styles.text}>{label}</Text>
+    </Pressable>
+  );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: Platform.OS === 'android' ? 'white' : '#4746AB',
+  },
+  button: {
+    backgroundColor: Platform.OS === 'android' ? '#5856D6' : 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  buttonPressed: {
+    backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
+  },
+});
--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Platform, Pressable, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { PrimaryButton } from '../components';
 
 export const CounterScreen = () => {
@@ -9,28 +9,15 @@ export const CounterScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>{count}</Text>
 
-      <PrimaryButton label="" onPress={} onLongPress={} />
-
-      <Pressable
+      <PrimaryButton
+        label="Incrementar"
         onPress={() => {
           setCount(count + 1);
         }}
         onLongPress={() => {
           setCount(0);
         }}
-        style={({ pressed }) => [
-          styles.button,
-          pressed && styles.buttonPressed,
-        ]}
-      >
-        <Text
-          style={{
-            color: Platform.OS === 'android' ? 'white' : '#4746AB',
-          }}
-        >
-          Incrementar
-        </Text>
-      </Pressable>
+      />
     </View>
   );
 };
@@ -45,14 +32,5 @@ const styles = StyleSheet.create({
     fontSize: 80,
     color: 'black',
     fontWeight: '300',
-  },
-  button: {
-    backgroundColor: Platform.OS === 'android' ? '#5856D6' : 'white',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 10,
-  },
-  buttonPressed: {
-    backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
   },
 });
