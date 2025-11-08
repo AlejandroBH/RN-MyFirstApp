67402708e230ba81cee4261b2c5353b84d62b838
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-11-08 15:57:34 -0300

    icons android

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { Text, View } from 'react-native';
 import { globalStyles } from '../theme/global.styles';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import { FAB } from 'react-native-paper';
 
 export const CounterM3Screen = () => {
@@ -9,11 +12,15 @@ export const CounterM3Screen = () => {
   return (
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{count}</Text>
+
+      <Icon name="accessibility-outline" size={35} />
+
       <FAB
         label="+1"
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
         style={globalStyles.fab}
+        // icon={() => <Icon name="accessibility-outline" size={25} />}
       />
     </View>
   );
