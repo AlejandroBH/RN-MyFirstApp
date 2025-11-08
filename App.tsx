113f070a5dbe9e5866ba9ea2c4ad543f7155bc9a
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-11-08 15:37:16 -0300

    install paper components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { SafeAreaView } from 'react-native';
 // import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen';
-import { CounterScreen } from './src/presentation/screens/CounterScreen';
+// import { CounterScreen } from './src/presentation/screens/CounterScreen';
+import { PaperProvider } from 'react-native-paper';
+import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <HelloWorldScreen name="Alejandro Barrera" /> */}
-      <CounterScreen />
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        {/* <HelloWorldScreen name="Alejandro Barrera" /> */}
+        {/* <CounterScreen /> */}
+        <CounterM3Screen />
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
